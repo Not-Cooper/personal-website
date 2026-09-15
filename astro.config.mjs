dfe9afc7a9ade2ddfa83f7b9e0f618e9bad0d0a0
Zach <zachb0209@gmail.com>
2026-09-14 21:56:53 -0400

    deployment changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [preact()]
+  site: "https://not-cooper.github.io",
+  base: "/personal-website",
+  integrations: [preact()],
 });
